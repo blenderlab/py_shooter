4250d90334f59733da223f12f4e46e9ce8f5f703
blenderlab <thomas@blenderlab.fr>
2021-12-03 09:57:38 +0100

    resizing  pix

--- settings.py
+++ settings.py
@@ -6,7 +6,11 @@ FRAME_RATE = 30
 WIDTH = 600
 HEIGHT = 600
 
-MYCARPIX = pygame.image.load("./data/ship.png")
+
+MYCARPIX = pygame.transform.scale((pygame.image.load("./data/ship.png")),(50,50))
+MYCARPIX = pygame.transform.scale(MYCARPIX,(50,50))
+
+
 ENNEMYPIX = pygame.image.load("./data/ennemy.png")
 
-MAX_ENNEMIES=20
+MAX_ENNEMIES=10
