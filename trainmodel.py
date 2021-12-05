bd5814f2937ec78789195dc4a01a12643d8f44cc
blenderlab <thomas@blenderlab.fr>
2021-12-05 08:22:14 +0100

    cleaining a bit

--- trainmodel.py
+++ trainmodel.py
@@ -19,28 +19,36 @@ for row  in csvReader :
 x_test = x_train[0:100]
 y_test = y_train[0:100]                       
 
+print (x_test[0:10])
+print (y_test[0:10])
+
 #2 create the model :
 
 model = keras.models.Sequential()
 
 # We have 4 inputs : Xpositiont, Left ennemies, Center Ennemies, Right Ennemies
-model.add(keras.layers.Dense(units=4,input_shape=(4,1)))
+model.add(keras.layers.Dense(units=4,input_shape=(4,1),input_dim=1))
 
-# Adding a 6 nerons hidden layer
-model.add(keras.layers.Dense(6, activation="relu"))
+# Adding 8 nerons hidden layer
+model.add(keras.layers.Dense(6,activation="relu"))
+model.add(keras.layers.Dense(4,activation="relu"))
+model.add(keras.layers.Dense(4,activation="relu"))
 
 # OUtput with a single output  : Action 
-model.add(keras.layers.Dense(units=1, activation="softmax"))
+model.add(keras.layers.Dense(1,activation="sigmoid"))
 
+# 3 Train it!
 
-model.compile(loss="sparse_categorical_crossentropy",optimizer="adam", metrics=["accuracy"])
+model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy']) 
 
 #print model summary 
 model.summary()
 
 # 3 Train it!
-model_history = model.fit(x_train,y_train,batch_size=1,epochs=10,validation_data=(x_test,y_test))
+model_history = model.fit(x_train,y_train,batch_size=10,epochs=10,validation_data=(x_test,y_test))
 # Check model efficiency : 
-model.evaluate(x_test,y_test,batch_size=1)
+model.evaluate(x_test,y_test,batch_size=100)
+print(model.predict(x_test[0:1]))
+
 # save it : 
 model.save(MODELNAME)
